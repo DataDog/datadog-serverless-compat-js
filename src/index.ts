c241ce5d753a53e75ca77f0f4415c0bb6038d04c
Kathie Huang <kathie.huang@datadoghq.com>
2025-09-28 22:33:44 -0400

    Check for DD_AZURE_RESOURCE_GROUP env var in Azure flex consumption functions

--- src/index.ts
+++ src/index.ts
@@ -53,6 +53,10 @@ function getBinaryPath(): string {
   );
 }
 
+function isAzureFlexWithoutDDAzureResourceGroup(): boolean {
+  return (process.env.WEBSITE_SKU === "FlexConsumption" && process.env.DD_AZURE_RESOURCE_GROUP === undefined);
+}
+
 function start(logger: Logger = defaultLogger): void {
   const environment = getEnvironment();
   logger.debug(`Environment detected: ${environment}`);
@@ -69,6 +73,13 @@ function start(logger: Logger = defaultLogger): void {
   if (process.platform !== 'win32' && process.platform !== 'linux') {
     logger.error(
       `Platform ${process.platform} detected, the Datadog Serverless Compatibility Layer is only supported on Windows and Linux`
+    );
+    return;
+  }
+
+  if (isAzureFlexWithoutDDAzureResourceGroup()) {
+    logger.error(
+      "Azure function detected on flex consumption plan without DD_AZURE_RESOURCE_GROUP set. Please set the DD_AZURE_RESOURCE_GROUP environment variable to your resource group name in Azure app settings. Shutting down Datadog Serverless Compatibility Layer."
     );
     return;
   }
